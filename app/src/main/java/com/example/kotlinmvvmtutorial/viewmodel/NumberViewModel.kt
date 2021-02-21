a197a82785b89ca62a3b418d14c6f93b036aa6c3
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-02-21 22:49:39 +0900

    CREATE | Create NumberViewModel & MainActivity UI

---
+++ app/src/main/java/com/example/kotlinmvvmtutorial/viewmodel/NumberViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.kotlinmvvmtutorial.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+enum class ActionType {
+    PLUS,
+    MINUS
+}
+
+class NumberViewModel : ViewModel() {
+
+    private val _currentValue = MutableLiveData<Int>()
+
+    val currentValue : LiveData<Int>
+        get() = _currentValue
+
+    init {
+        _currentValue.value = 0
+    }
+
+    fun updateValue(actionType: ActionType, input: Int) {
+        when(actionType) {
+            ActionType.PLUS ->
+                _currentValue.value = _currentValue.value?.plus(input)
+            ActionType.MINUS ->
+                _currentValue.value = _currentValue.value?.minus(input)
+        }
+    }
+}
