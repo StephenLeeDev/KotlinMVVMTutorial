c94afff04ce2c54a392800cf8af7447243cace73
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-02-21 23:04:52 +0900

    EDIT | Migrate to DataBinding

--- app/src/main/java/com/example/kotlinmvvmtutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmvvmtutorial/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.kotlinmvvmtutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.kotlinmvvmtutorial.databinding.ActivityMainBinding
+import com.example.kotlinmvvmtutorial.viewmodel.NumberViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var numberViewModel: NumberViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.activity = this
+
+        binding.buttonPlus.setOnClickListener(this)
+        binding.buttonMinus.setOnClickListener(this)
+
+        numberViewModel = ViewModelProvider(this).get(NumberViewModel::class.java)
+        numberViewModel.currentValue.observe(this, Observer {
+
+        })
+    }
+
+    override fun onClick(v: View?) {
+        when(v) {
+
+        }
     }
 }
